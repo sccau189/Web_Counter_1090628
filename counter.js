bc5b8591e07085f16313bb5eec988813f84eff25
sccau189 <sccau189@hotmail.com>
2020-06-28 15:33:11 +0800

    js準備頁面完成

---
+++ counter.js
@@ -0,0 +1,9 @@
+$(document).ready( () => {
+
+    // var 變數 = 資料
+    var offset = $("#test").offset();     //取的元素位移資料
+    var top = offset.top;                 //上方
+    var left = offset.left;               //下方
+    
+    
+});
